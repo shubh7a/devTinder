6357b38b1fd3cc12e16f2107cc0346ba5e226302
Shubham Kumar Singh <shubham@example.com>
2026-01-17 21:18:43 +0530

    next() at routes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 app.use(
    "/users" , [
-      // only 2nd will run at postman 
+      // only 3rd will run at postman 
       (req,res,next)=>{
          console.log(" handling 1st router handler ");
          res.send(" 1st router handler ");
@@ -14,7 +14,7 @@ app.use(
       (req,res,next)=>{
          console.log(" handling 2nd router handler ");
          res.send(" 2nd router handler ");
-         
+         next();
       },
       (req,res,next)=>{
          console.log(" handling 3rd router handler ");
